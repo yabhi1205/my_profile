3ead5e19e5f793ecb587076e897ef9f3c1a39214
Yabhi1205 <83153764+yabhi1205@users.noreply.github.com>
2023-11-24 09:39:36 +0530

    Update about.js

--- app/Components/about.js
+++ app/Components/about.js
@@ -51,7 +51,7 @@ export default function About() {
                     <div id="cursor" className="h-full "></div>
                 </div>}
                 <div className="lg:w-7/12 p-4 max-lg:w-full container">
-                    <img className="mx-auto" src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" />
+                    <img className="mx-auto" src="./pictures/profile.jpg" />
                 </div>
             </div>
         </>
